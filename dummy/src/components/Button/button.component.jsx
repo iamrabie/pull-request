32832705127193f1f92278c93a1621f1c6e540b5
Rabia Khan <106153809+iamrabie@users.noreply.github.com>
2025-08-29 17:13:44 +0500

    created a button comp.

---
+++ dummy/src/components/Button/button.component.jsx
@@ -0,0 +1,12 @@
+const Button = () => {
+    return(
+        <>
+        <button style={{padding:'10px'}}>
+            Click me
+        </button>
+        </>
+    );
+}
+
+
+export default Button;
